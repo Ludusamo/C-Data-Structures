726845423ed557553489d9dc779cb45efb144293
Brendan Horng <brendan.horng@ludusamo.com>
2017-07-09 22:22:54 -0400

    Add object and value representation

--- header/obj.h
+++ header/obj.h
@@ -6,9 +6,6 @@ typedef struct Obj_S {
 	void *ptr;
 } Obj;
 
-void ctor_obj(Obj *obj, const char *type, void *ptr);
-void dtor_obj(Obj *obj);
-
 int is_type(const Obj *obj, const char *type);
 
 #endif // OBJ_H
--- header/value_test.h
+++ header/value_test.h
@@ -9,6 +9,7 @@ int value_test_teardown(void **state);
 void value_double_test(void **state);
 void value_int_test(void **state);
 void value_ptr_test(void **state);
+void value_obj_test(void **state);
 
 int run_value_tests();
 

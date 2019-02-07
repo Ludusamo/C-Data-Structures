08babee954f285cd310588c58a2d7e77902b8fb4
Brendan Horng <brendan.horng@ludusamo.com>
2019-02-06 22:36:36 -0500

    Remove obj from value

--- header/obj.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBJ_H
-#define OBJ_H
-
-typedef struct Obj_S {
-    int type;
-    void *ptr;
-} Obj;
-
-#endif // OBJ_H
--- src/value.c
+++ src/value.c
@@ -17,10 +17,6 @@ int is_int32(Value v) {
     return (v.bits & int_mask) == int_mask;
 }
 
-int is_obj(Value v) {
-    return (v.bits & obj_mask) == obj_mask;
-}
-
 Value from_double(double d) {
     Value v;
     if (d == (int32_t) d && !is_neg_zero(d)) {
@@ -37,16 +33,6 @@ Value from_ptr(void *p) {
     return (Value) { .bits = p_i | ptr_mask };
 }
 
-Value from_obj(Obj *obj) {
-    uintptr_t p_i = (uintptr_t) obj;
-    assert((p_i & ptr_mask) == 0);
-    return (Value) { .bits = p_i | obj_mask };
-}
-
 void *get_ptr(Value v) {
     return (void*) (v.bits & ~ptr_mask);
-}
-
-Obj *get_obj(Value v) {
-    return (Obj*) (v.bits & ~obj_mask);
 }
